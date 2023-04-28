ea6af296b5e4c60273dd4b51dca38d8fb115e1ff
MALWICK <batahumphrey66@gmail.com>
2023-04-28 10:58:23 +0100

    edit my button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
-import Landing from './view/pages/landing/Landing';
+/* import Landing from './view/pages/landing/Landing'; */
 // import AdminDashboard from './view/pages/admin/dashboard/AdminDashboard';
-// import Login from './view/pages/Login';
+import Login from './view/pages/Login';
 // import Signup from './view/pages/Signup';
 // import Button from './core/components/atoms/Button';
 // import NavBar from './core/components/organisms/NavBar';
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <div className="Appdiv">
-        <Landing />
+        <Login />
       </div>
     </div>
   );
