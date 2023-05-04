949cd32cc09fcf9022285f936ef57c156275f297
MALWICK <batahumphrey66@gamil.com>
2023-05-04 16:19:36 +0100

    did some changes on these baoard

--- src/view/pages/admin/quizzes/AdminQuizzes.jsx
+++ src/view/pages/admin/quizzes/AdminQuizzes.jsx
@@ -9,62 +9,64 @@ function AdminQuizzes() {
         <div className="admin__container">
           <form className="quiz__setterContianer">
             <div className="quiz__setterContianer1">
-              <div className="imgUrl__contai">
-                <div className="imgUrl__img">n</div>
-                <div className="img__text">
-                  <h3>Set image Url</h3>
-                  <span>Optional</span>
-                </div>
+              <div className="quiz__img">
+                <h2>Image Url</h2>
+                <input type="text" placeholder="set image url" />
               </div>
               <div className="quiz__tittle">
                 <h2>Quiz title</h2>
-                <button type="submit">Done</button>
                 <input type="text" placeholder="enter Quiz-Title" />
               </div>
               <div className="question__timer">
-                <h2>Timer per question</h2>
-                <button type="submit">Timer</button>
-                <input type="time" list="limittimesList" step="0.00" />
+                <h2>Timer for question</h2>
+                <input
+                  type="text"
+                  placeholder="set timer either per seconds 0:45s"
+                />
               </div>
             </div>
             <div className="each__question_container">
               <div className="firstQuestion__title">
-                <h2>Quiz Title</h2>
+                <h2>Question n:1</h2>
               </div>
               <div className="firstquestion">
                 <div className="question__radio">
-                  <input type="radio" name="option1" id="option1" />
+                  <p>Question</p>
+                </div>
+                <div className=" option1__input ">
+                  <input type="text" placeholder="write question here" />
+                </div>
+              </div>
+              <div className="firstquestion">
+                <div className="question__radio">
                   <p>Option1</p>
                 </div>
                 <div className=" option1__input ">
-                  <input type="text" placeholder="write question here" />
+                  <input type="text" placeholder="write first option here" />
                 </div>
               </div>
               <div className="firstquestion">
                 <div className="question__radio">
-                  <input type="radio" name="option1" id="option1" />
                   <p>Option2</p>
                 </div>
                 <div className=" option1__input ">
-                  <input type="text" placeholder="write question here" />
+                  <input type="text" placeholder="write second option here" />
                 </div>
               </div>
               <div className="firstquestion">
                 <div className="question__radio">
-                  <input type="radio" name="option1" id="option1" />
                   <p>Option3</p>
                 </div>
                 <div className=" option1__input ">
-                  <input type="text" placeholder="write question here" />
+                  <input type="text" placeholder="write third option here" />
                 </div>
               </div>
               <div className="firstquestion">
                 <div className="question__radio">
-                  <input type="radio" name="option1" id="option1" />
                   <p>Option4</p>
                 </div>
                 <div className=" option1__input ">
-                  <input type="text" placeholder="write question here" />
+                  <input type="text" placeholder="write forth option here" />
                 </div>
               </div>
               <div>
