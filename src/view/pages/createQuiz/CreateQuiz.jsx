789476f049eaa4722ad2157f295e8afe9d62ca07
NNVG <g696607013@gmail.com>
2023-05-06 04:32:42 +0100

    Setting the quiz qyestions

--- src/view/pages/createQuiz/CreateQuiz.jsx
+++ src/view/pages/createQuiz/CreateQuiz.jsx
@@ -1,22 +1,38 @@
-/* eslint-disable no-console */
-/* eslint-disable no-undef */
-
-import React from 'react';
+/* eslint-disable react/prop-types */ /* eslint-disable no-console */ /*
+eslint-disable no-undef */
+import React, { useState } from 'react';
+import AdminTemplate from '../../templates/admin/admintemplate/AdminTemplate';
 import './createQuiz.css';
 import { setQuiz } from '../../../api/auth';
-import AdminTemplate from '../../templates/admin/admintemplate/AdminTemplate';
+import AuthGuard from '../admin/profile/AuthGuard';
 
-function CreateQuiz() {
+function CreateQuiz({ user }) {
+  const [question, setQuestion] = useState('');
+  const [option1, setOption1] = useState('');
+  const [option2, setOption2] = useState('');
+  const [option3, setOption3] = useState('');
+  const [option4, setOption4] = useState('');
+  const [disabled, setDisabled] = useState(false);
+  const [selected, setSelected] = useState(false);
+  const [num, setNum] = useState(1);
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const values = Object.fromEntries(data.entries());
-    console.log(values);
     await setQuiz(values);
-    console.log(values);
+    setQuestion('');
+    setOption1('');
+    setOption2('');
+    setOption3('');
+    setOption4('');
+    setNum(num + 1);
+    console.log(values, selected, setSelected);
+    if (num === 10) {
+      setDisabled(true);
+    }
   };
   return (
-    <AdminTemplate>
+    <AdminTemplate page="CreateQuiz">
       <div className="quiz">
         <div className="admin__container">
           <form onSubmit={handleSubmit} className="quiz__setterContianer">
@@ -34,6 +50,7 @@ function CreateQuiz() {
                 <input
                   name="quizName"
                   type="text"
+                  required
                   placeholder="enter Quiz-Title"
                 />
               </div>
@@ -42,13 +59,14 @@ function CreateQuiz() {
                 <input
                   name="quizTimer"
                   type="text"
+                  required
                   placeholder="set timer in seconds 0:45s"
                 />
               </div>
             </div>
             <div className="each__question_container">
               <div className="firstQuestion__title">
-                <h2>Question No.1</h2>
+                <h2>Question No.{num}</h2>
               </div>
               <div className="firstquestion">
                 <div className="question__radio">
@@ -59,6 +77,9 @@ function CreateQuiz() {
                     name="question"
                     className="input__question"
                     type="text"
+                    required
+                    onChange={(event) => setQuestion(event.target.value)}
+                    value={question}
                     placeholder="write question here"
                   />
                 </div>
@@ -72,6 +93,9 @@ function CreateQuiz() {
                     className="input__option"
                     name="option1"
                     type="text"
+                    required
+                    onChange={(event) => setOption1(event.target.value)}
+                    value={option1}
                     placeholder="write first option here"
                   />
                 </div>
@@ -85,6 +109,9 @@ function CreateQuiz() {
                     className="input__option"
                     name="option2"
                     type="text"
+                    required
+                    onChange={(event) => setOption2(event.target.value)}
+                    value={option2}
                     placeholder="write second option here"
                   />
                 </div>
@@ -98,6 +125,9 @@ function CreateQuiz() {
                     className="input__option"
                     name="option3"
                     type="text"
+                    required
+                    onChange={(event) => setOption3(event.target.value)}
+                    value={option3}
                     placeholder="write third option here"
                   />
                 </div>
@@ -111,6 +141,9 @@ function CreateQuiz() {
                     className="input__option"
                     name="option4"
                     type="text"
+                    required
+                    onChange={(event) => setOption4(event.target.value)}
+                    value={option4}
                     placeholder="write forth option here"
                   />
                 </div>
@@ -120,24 +153,36 @@ function CreateQuiz() {
                   <p>Correct Answer</p>
                 </div>
                 <div className=" select__correctanswer ">
-                  <select name="Select correct answer" id="correctAnswer">
-                    <option value="">
-                      Select correct Answer from the options above
+                  <select required name="answer">
+                    <option value="{selected}">
+                      Select correct Answer from the options below
                     </option>
-                    <option value="option1">Option1</option>
-                    <option value="option2">Option2</option>
-                    <option value="option3">Option3</option>
-                    <option value="option4">Option4</option>
+                    <option value="{option1}">{option1}</option>
+                    <option value="{option2}">{option2}</option>
+                    <option value="{option3}">{option3}</option>
+                    <option value="{option4}">{option4}</option>
                   </select>
                 </div>
               </div>
               <div className="btn__container">
-                <button type="submit" className="next__btn">
+                <button type="submit" disabled={disabled} className="next__btn">
                   Next Question
                 </button>
-                <button type="submit" className="sub__btn">
-                  submit
-                </button>
+                {/* <button type="submit" className="sub__btn">submit</button> */}
+              </div>
+              <div className="firstquestion">
+                <div className="question__radio">
+                  <p hidden>UserId</p>
+                </div>
+                <div className=" option1__input ">
+                  <input
+                    className="input__option"
+                    name="UserId"
+                    type="number"
+                    hidden
+                    defaultValue={user.id}
+                  />
+                </div>
               </div>
             </div>
           </form>
@@ -146,5 +191,4 @@ function CreateQuiz() {
     </AdminTemplate>
   );
 }
-
-export default CreateQuiz;
+export default AuthGuard(CreateQuiz);
