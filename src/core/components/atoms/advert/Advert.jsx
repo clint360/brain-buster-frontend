acffd06fd7bcc11934b9a5f7b4b67a19f7507a3e
Boanong Joshua <boanongjoshua@gmail.com>
2023-05-08 21:12:52 +0100

    Adjust borders

---
+++ src/core/components/atoms/advert/Advert.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './advert.css';
+import Rebase from '../../../../assets/images/rebase.svg';
+import Lenonovo from '../../../../assets/images/lenovo.svg';
+import Mtn from '../../../../assets/images/mtn.svg';
+import Cocacola from '../../../../assets/images/cocacola.svg';
+import Camaico from '../../../../assets/images/camairco.svg';
+
+function Advert() {
+  return (
+    <div className="advertContainer">
+      <h1>Trusted By</h1>
+      <div className="ad__img">
+        <img src={Rebase} alt="" />
+        <img src={Lenonovo} alt="" />
+        <img src={Mtn} alt="" />
+        <img src={Cocacola} alt="" />
+        <img src={Camaico} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Advert;
---
+++ src/models/quizzes.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+const response = {
+  username: '',
+  email: '',
+  quiz: '',
+  answerIndices: [],
+};
+
+const quiz = {
+  admin: 's',
+  name: '',
+  duration: '',
+  responses: [response, response, response],
+  quizImageURL: '',
+};
+
+const questionTemplete = {
+  question: '',
+  options: ['', '', '', ''],
+  answerIndex: 0,
+  userAnswerIndex: null,
+};
