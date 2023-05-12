c76f9d5f0eb13902d4dd59db6abb61d23918276e
clint360 <116176590+clint360@users.noreply.github.com>
2023-05-12 16:50:50 +0100

    Update AdminDashboard.jsx

--- src/view/pages/admin/dashboard/AdminDashboard.jsx
+++ src/view/pages/admin/dashboard/AdminDashboard.jsx
@@ -6,6 +6,24 @@ import AdminTemplate from '../../../templates/admin/admintemplate/AdminTemplate'
 import './AdminDashboard.css';
 import AuthGuard from '../profile/AuthGuard';
 
+function createHex() {
+  let hexCode1 = '';
+  const hexValues1 = '0123456789abcdef';
+  for (let i = 0; i < 6; i++) {
+    hexCode1 += hexValues1.charAt(
+      Math.floor(Math.random() * hexValues1.length)
+    );
+  }
+  return hexCode1;
+}
+
+function generate() {
+  const deg = Math.floor(Math.random() * 360);
+  const gradient =
+    `linear-gradient(${deg}deg, ` + `#${createHex()}, ` + `#${createHex()})`;
+  return gradient;
+}
+
 function AdminDashboard({ user }) {
   return (
     <AdminTemplate
