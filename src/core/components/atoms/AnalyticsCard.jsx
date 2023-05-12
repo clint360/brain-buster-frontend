c5ff3a0de470a161e0ca74158aaf414880779677
clint360 <116176590+clint360@users.noreply.github.com>
2023-05-12 15:59:56 +0100

    Create AnalyticsCard.jsx

---
+++ src/core/components/atoms/AnalyticsCard.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable object-shorthand */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/prop-types */
+import React, { useEffect } from 'react';
+
+function AnalyticsCard({ number, title, backgroundGradient }) {
+  useEffect(() => {
+    console.log(backgroundGradient());
+  });
+
+  const cardStyle = {
+    height: '125px',
+    width: '125px',
+    background: backgroundGradient(),
+    borderRadius: '10px',
+    textAlign: 'center',
+    fontFamily: 'Commissioner',
+    fontWeight: 'bold',
+    color: 'white',
+    padding: '10px',
+    overflow: 'hidden',
+    cursor: 'pointer',
+  };
+
+  const imgprevStyle = {
+    height: '80px',
+    borderTopLeftRadius: '10px',
+    borderTopRightRadius: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '50px',
+  };
+
+  const imgStyle = {
+    borderRadius: '50%',
+    width: '70px',
+    height: '70px',
+  };
+
+  return (
+    <div style={cardStyle} className="adminquizcard">
+      <div className="imgprev" style={imgprevStyle}>
+        <div style={imgStyle}> {number} </div>
+      </div>
+      <div className="quizname">{title || '?'}</div>
+    </div>
+  );
+}
+
+export default AnalyticsCard;
