bc629a76dfb70add5eac6662e32cc9f09392a7b6
clint360 <116176590+clint360@users.noreply.github.com>
2023-05-12 16:02:34 +0100

    Update AdminDashboard.jsx

--- src/view/pages/admin/dashboard/AdminDashboard.jsx
+++ src/view/pages/admin/dashboard/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import AdminQuizCard from '../../../../core/components/atoms/AdminQuizCard';
+import AnalyticsCard from '../../../../core/components/atoms/AnalyticsCard';
 import AdminTemplate from '../../../templates/admin/admintemplate/AdminTemplate';
 import './AdminDashboard.css';
 import AuthGuard from '../profile/AuthGuard';
@@ -12,7 +13,36 @@ function AdminDashboard({ user }) {
     and activity summary"
       username={user.fullName}
       page="AdminDashboard"
-    >
+    ><div className="createnewquiz">
+        <div>
+          <i className="fa-sharp fa-solid fa-circle-plus fa-bounce" />
+          <div>Create New Quiz</div>
+        </div>
+      </div>
+      <div className="analytics" />
+      <div className="displayquizzes">
+        <h1>Analytics</h1>
+        <div className="quizzesslides">
+          <div>
+            <AnalyticsCard
+              title="Total Number of Responses"
+              number="23"
+              backgroundGradient={() => {
+                return generate();
+              }}
+            />
+          </div>
+          <div>
+            <AnalyticsCard
+              title="Total Number of Responses"
+              number="23"
+              backgroundGradient={() => {
+                return generate();
+              }}
+            />
+          </div>
+        </div>
+      </div>
       <div className="displayquizzes">
         <h1>Your Quizzes</h1>
         <div className="quizzesslides">
@@ -27,12 +57,6 @@ function AdminDashboard({ user }) {
           </div>
           <div>
             <AdminQuizCard quizName="F2 Biology Paper1" />
-          </div>
-        </div>
-        <div className="createnewquiz">
-          <div>
-            <i className="fa-sharp fa-solid fa-circle-plus fa-bounce" />
-            <div>Create New Quiz</div>
           </div>
         </div>
       </div>
